c9b2aabe0a8fc1ba70a887b318bcc5dec1712e09
jjinsama <jjinsama@gmail.com>
2016-01-23 11:35:57 +0900

    add file

--- SmartAlert/src/main/java/com/namleesin/smartalert/main/MainValue.java
+++ SmartAlert/src/main/java/com/namleesin/smartalert/main/MainValue.java
@@ -2,9 +2,8 @@ package com.namleesin.smartalert.main;
 
 public class MainValue 
 {
-	public static final int RES_SPLASH_SCREEN 				= 0;
+	public static final int RES_SPLASH_SCREEN 			= 0;
 	public static final int RES_GUIDE_WIZARD				= 1;
 	public static final int RES_ALERT_SETTING				= 2;
-	public static final int RES_SPAM_SETTING				= 3;
-	public static final int RES_LIKE_SETTING				= 4;
+	public static final int RES_SL_SETTING					= 3;
 }
